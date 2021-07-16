9ee28b3b9d33df0c384b0dd81c658afcdc131328
rgalhos <rgmg@ic.ufal.br>
2021-07-16 14:03:48 -0300

    Minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
 import Schema from "./classes/Schema";
 
 export default Schema;
-
-const x = new Schema(false);
-
-x.load().then(() => {
-    x.downloadFullItemSchema().then(() => {
-        x.dumpSchema("./schema.json");
-    });
-});
